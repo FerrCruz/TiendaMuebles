4063aefc3561048d89a9d18caffc38dddfdb4a18
FerrCruz <cfer3890@gmail.com>
2023-10-06 23:26:34 -0300

    Agregado de paginas, contenido, y formulario

---
+++ a.c
@@ -0,0 +1,23 @@
+[33mcommit b3dd7cb201f629d9fe9c122e314a8ebfd6a7167c[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: FerrCruz <cfer3890@gmail.com>
+Date:   Fri Oct 6 02:58:44 2023 -0300
+
+    rediseñado productoscon grid, agregado de footer
+
+[33mcommit 0fdd78c0aa4b87f0ebbff73db43095aee3a50801[m
+Author: FerrCruz <cfer3890@gmail.com>
+Date:   Wed Oct 4 00:42:07 2023 -0300
+
+    Centrado de textos,imgs,agregado de rems fontSize
+
+[33mcommit 618fc014c842be4bf2ac0feaa47217d88cafd893[m
+Author: FerrCruz <cfer3890@gmail.com>
+Date:   Tue Oct 3 00:54:22 2023 -0300
+
+    Agregado de index y nosotros html e imagenes
+
+[33mcommit 4a080766dbaea4d6dcea8c05d1065a6999959018[m
+Author: FerrCruz <54588781+FerrCruz@users.noreply.github.com>
+Date:   Tue Oct 3 00:52:43 2023 -0300
+
+    Initial commit
